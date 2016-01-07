d6152a54c6d2a7e9434cf32b9b20cd916201b1bb
ggianna <ggianna@scify.org>
2016-01-07 15:35:33 +0200

    - Added Apache 2.0 license header to all files. - Updated HmmAnalyzer to support list of <observation lists>. - Provided a more compact versio of the analyze method of HmmAnalyzer (using iterators).

--- Documents/NetBeansProjects/CpGIslandDetection/src/gr/demokritos/iit/cpgislanddetection/analysis/HmmAnalyzer.java
+++ Documents/NetBeansProjects/CpGIslandDetection/src/gr/demokritos/iit/cpgislanddetection/analysis/HmmAnalyzer.java
@@ -5,40 +5,12 @@
  */
 package gr.demokritos.iit.cpgislanddetection.analysis;
 
-import be.ac.ulg.montefiore.run.distributions.GaussianDistribution;
 import java.util.*;
-import be.ac.ulg.montefiore.run.jahmm.Hmm;
-import be.ac.ulg.montefiore.run.jahmm.Observation;
 import be.ac.ulg.montefiore.run.jahmm.ObservationDiscrete;
-import be.ac.ulg.montefiore.run.jahmm.ObservationVector;
-import be.ac.ulg.montefiore.run.jahmm.OpdfDiscrete;
-import be.ac.ulg.montefiore.run.jahmm.OpdfDiscreteFactory;
-import be.ac.ulg.montefiore.run.jahmm.draw.GenericHmmDrawerDot;
-import be.ac.ulg.montefiore.run.jahmm.io.FileFormatException;
-import be.ac.ulg.montefiore.run.jahmm.io.ObservationSequencesReader;
-import be.ac.ulg.montefiore.run.jahmm.io.ObservationVectorReader;
-import be.ac.ulg.montefiore.run.jahmm.learn.BaumWelchLearner;
-import be.ac.ulg.montefiore.run.jahmm.toolbox.KullbackLeiblerDistanceCalculator;
-import be.ac.ulg.montefiore.run.jahmm.toolbox.MarkovGenerator;
 import gr.demokritos.iit.cpgislanddetection.entities.BaseSequence;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
-import static javax.management.Query.value;
-import java.io.ObjectOutputStream;
-import java.util.Vector;
-import static javafx.scene.input.KeyCode.O;
 import gr.demokritos.iit.cpgislanddetection.entities.HmmSequence.Packet;
-import gr.demokritos.iit.cpgislanddetection.entities.HmmSequence;
 
 /**
  * HmmAnalyzer creates a sequence of A,T,C,G from a List of Strings or
@@ -47,44 +19,68 @@ import gr.demokritos.iit.cpgislanddetection.entities.HmmSequence;
  *
  * @author Xenia
  */
-public class HmmAnalyzer implements ISequenceAnalyst<List<ObservationDiscrete<Packet>>> {
+public class HmmAnalyzer implements ISequenceAnalyst<List<List<ObservationDiscrete<Packet>>>> {
 
     @Override
-    public List<ObservationDiscrete<Packet>> analyze(ArrayList<BaseSequence> baseSeq) {
+    public List<List<ObservationDiscrete<Packet>>> analyze(ArrayList<BaseSequence> baseSeq) {
 
-        List<ObservationDiscrete<Packet>> testSeq
-                = new ArrayList<ObservationDiscrete<Packet>>();
-
-        ObservationDiscrete<Packet> packetA = Packet.A.observation();
-        ObservationDiscrete<Packet> packetT = Packet.T.observation();
-        ObservationDiscrete<Packet> packetC = Packet.C.observation();
-        ObservationDiscrete<Packet> packetG = Packet.G.observation();
-
-        for (int line = 0; line < baseSeq.size(); line++) {
-            String currentStr = baseSeq.get(line).getSymbolSequence();
-            System.out.println(currentStr);
-            for (int i = 0; i < baseSeq.get(line).myLength(currentStr); i++) {
-                if (baseSeq.get(line).myCharAt(i, currentStr) == 'A') {
-
-                    testSeq.add(packetA);
-
-                } else if (baseSeq.get(line).myCharAt(i, currentStr) == 'T') {
-
-                    testSeq.add(packetT);
-
-                } else if (baseSeq.get(line).myCharAt(i, currentStr) == 'C') {
-
-                    testSeq.add(packetC);
-
-                } else if (baseSeq.get(line).myCharAt(i, currentStr) == 'G') {
-
-                    testSeq.add(packetG);
-
-                }
+        List<List<ObservationDiscrete<Packet>>> lRes = new ArrayList<>();
+                
+        // Map strings to observations
+        Map<Character,ObservationDiscrete<Packet>> mMap = new HashMap<>();
+        mMap.put('A',Packet.A.observation());
+        mMap.put('G',Packet.G.observation());
+        mMap.put('T',Packet.T.observation());
+        mMap.put('C',Packet.C.observation());
+        mMap.put('a',Packet.A.observation());
+        mMap.put('g',Packet.G.observation());
+        mMap.put('t',Packet.T.observation());
+        mMap.put('c',Packet.C.observation());
+        
+        // For each sequence
+        for (BaseSequence bsCur : baseSeq) {
+            List<ObservationDiscrete<Packet>> testSeq = new ArrayList<>();
+            // For each character
+            for (Character c: bsCur.getSymbolSequence().toCharArray()) {
+                testSeq.add(mMap.get(c));
             }
-            System.out.println(testSeq);
+            
+            // Output observation list
+            System.err.println(testSeq);
+            // Add observation list to list of sequences (result list)
+            lRes.add(testSeq);
         }
 
-        return testSeq;
+//        ObservationDiscrete<Packet> packetA = Packet.A.observation();
+//        ObservationDiscrete<Packet> packetT = Packet.T.observation();
+//        ObservationDiscrete<Packet> packetC = Packet.C.observation();
+//        ObservationDiscrete<Packet> packetG = Packet.G.observation();
+//        
+//        for (int line = 0; line < baseSeq.size(); line++) {
+//            String currentStr = baseSeq.get(line).getSymbolSequence();
+//            System.out.println(currentStr);
+//            for (int i = 0; i < baseSeq.get(line).myLength(currentStr); i++) {
+//                if (baseSeq.get(line).myCharAt(i, currentStr) == 'A') {
+//
+//                    testSeq.add(packetA);
+//
+//                } else if (baseSeq.get(line).myCharAt(i, currentStr) == 'T') {
+//
+//                    testSeq.add(packetT);
+//
+//                } else if (baseSeq.get(line).myCharAt(i, currentStr) == 'C') {
+//
+//                    testSeq.add(packetC);
+//
+//                } else if (baseSeq.get(line).myCharAt(i, currentStr) == 'G') {
+//
+//                    testSeq.add(packetG);
+//
+//                }
+//            }
+//            System.out.println(testSeq);
+//        }
+
+        return lRes;
     }
 }
