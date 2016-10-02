bece8e08b5d441febde393815b783130f3b51bcc
Xenia-Io <ksenia_io@hotmail.com>
2016-10-02 18:26:45 +0300

    java code for vector's classification

--- Documents/NetBeansProjects/CpGIslandDetection/src/gr/demokritos/iit/cpgislanddetection/CpGIslandDetection.java
+++ Documents/NetBeansProjects/CpGIslandDetection/src/gr/demokritos/iit/cpgislanddetection/CpGIslandDetection.java
@@ -22,18 +22,28 @@ import gr.demokritos.iit.cpgislanddetection.analysis.ISequenceAnalyst;
 import gr.demokritos.iit.cpgislanddetection.analysis.ISequenceClassifier;
 import gr.demokritos.iit.cpgislanddetection.analysis.NGramGraphAnalyzer;
 import gr.demokritos.iit.cpgislanddetection.analysis.NGramGraphClassifier;
+import gr.demokritos.iit.cpgislanddetection.analysis.VectorAnalyzer;
 import gr.demokritos.iit.cpgislanddetection.entities.BaseSequence;
 import gr.demokritos.iit.cpgislanddetection.entities.HmmSequence;
 import gr.demokritos.iit.cpgislanddetection.io.FASTAFileReader;
 import gr.demokritos.iit.cpgislanddetection.io.FASTAObfuscatorReader;
+import gr.demokritos.iit.cpgislanddetection.io.FileCreatorARFF;
 import gr.demokritos.iit.cpgislanddetection.io.SequenceListFileReader;
 import gr.demokritos.iit.cpgislanddetection.io.IGenomicSequenceFileReader;
 import gr.demokritos.iit.jinsect.documentModel.representations.DocumentNGramGraph;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Array;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
+import weka.classifiers.*;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.bayes.NaiveBayesUpdateable;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
 
 /**
  *
@@ -44,41 +54,115 @@ public class CpGIslandDetection {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException, Exception {
+        
+       // String sFileNameArgs = args[0];
 
-        String sFileNameArgs = args[0];
-
-        String[] fileNames = null;
+       // String[] fileNames = null;
         // Read  file
         //IGenomicSequenceFileReader reader = new SequenceListFileReader();
-        IGenomicSequenceFileReader reader = new FASTAFileReader();
-        ArrayList<BaseSequence> lSeqs;
+        
+//        String seq ="GCTCTTGACTTTCAGACTTCCTGAAAACAACGTTCTGGTAAGGACAAGGGTT";
+//
+//        CpGIslandIdentification iClass = new CpGIslandIdentification();
+//        boolean b = iClass.identify(seq);
+//        System.out.println("This sequence is a CpG island: " + b);
+//        SequenceListFileReader s = new SequenceListFileReader();
+//        ArrayList<BaseSequence> alRes = new ArrayList<>();
+//        
+//        alRes = s.getSequencesFromFile("C:\\Users\\Xenia\\Desktop\\files\\posSamples.txt");
+
+//        for(int i=0; i<alRes.size(); i++)
+//        System.out.println("alRes = " + i + alRes.get(i));
+//        VectorAnalyzer vA = new VectorAnalyzer();
+//        List<Vector<Integer>> listVector = new ArrayList<>();
+        //Vector<Vector<Integer>> list = 
+//        listVector = vA.analyze(alRes);
+//        for(int i=0; i<listVector.size();i++)
+//        System.out.println(i + " " +listVector.get(i));
+        //IGenomicSequenceFileReader reader = new FASTAFileReader();
+        
+        
+        
         // If no input file has been given
-        if (args.length == 0) {
+/*        if (args.length == 0) {
             // Use default
-            fileNames[0] = "C:\\Users\\Xenia\\Desktop\\posSamples.txt";
-            fileNames[1] = "C:\\Users\\Xenia\\Desktop\\negSamples.txt";
-            fileNames[2] = "C:\\Users\\Xenia\\Desktop\\newsamples.txt";
+            fileNames[0] = "C:\\Users\\Xenia\\Desktop\\files\\posSamples.txt";
+            fileNames[1] = "C:\\Users\\Xenia\\Desktop\\files\\negSamples.txt";
+            fileNames[2] = "C:\\Users\\Xenia\\Desktop\\files\\newsamples.txt";
 
         } else // else use the provided one
         {
             fileNames = sFileNameArgs.split(";");
         }
+*/  
+        
+        
+        //-----------------VECTOR ANALYSIS STARTS HERE--------------------------------------
+        
+        //read sequences from txt files
+        SequenceListFileReader reader = new SequenceListFileReader();
+        ArrayList<BaseSequence> lSeqs1 = new ArrayList<>();
+        ArrayList<BaseSequence> lSeqs2 = new ArrayList<>();
+        
+        lSeqs1 = reader.getSequencesFromFile("C:\\Users\\Xenia\\Desktop\\files\\posSamples.txt");
+        lSeqs2 = reader.getSequencesFromFile("C:\\Users\\Xenia\\Desktop\\files\\negSamples.txt");
+       
+        //create vectors for every sequence
+        List<Vector<Integer>> listVectorForPositiveSamples = new ArrayList<>();
+        List<Vector<Integer>> listVectorForNegativeSamples = new ArrayList<>();
+        VectorAnalyzer v = new VectorAnalyzer();
+        listVectorForPositiveSamples = v.analyze(lSeqs1);
+        listVectorForNegativeSamples = v.analyze(lSeqs2);
+        
+        //create ARFF files for positive and negative samples
+        FileCreatorARFF fc = new FileCreatorARFF();
+        Instances positiveInstances = fc.createARFF(listVectorForPositiveSamples,"yes");
+        Instances negativeInstances = fc.createARFF(listVectorForNegativeSamples,"no");
+        //System.out.println(positiveInstances);
+        
+        //build and train classifier
+        // setting class attribute
+        positiveInstances.setClassIndex(positiveInstances.numAttributes() - 1);
+        negativeInstances.setClassIndex(negativeInstances.numAttributes() - 1);
+        // train NaiveBayes
+        NaiveBayesUpdateable nb = new NaiveBayesUpdateable();
+        nb.buildClassifier(positiveInstances);
+        nb.buildClassifier(negativeInstances);
+        Instance current;
+        for(int i=0; i<positiveInstances.numInstances(); i++){
+            current = positiveInstances.instance(i);
+            nb.updateClassifier(current);
+        }
+        
+        // Test the model
+        Evaluation eTest = new Evaluation(positiveInstances);
+        Instances isTestingSet = fc.createARFF(listVectorForNegativeSamples,"?");
+        isTestingSet.setClassIndex(isTestingSet.numAttributes() - 1);
+        eTest.evaluateModel(nb, isTestingSet);
+        
+        //------------------VECTOR ANALYSIS ENDS HERE---------------------------------------
+        
+        
+        
+        
+        
+        //----------------------------HMM CLASSIFIER STARTS HERE----------------------------------
         // Init classifier
-        ISequenceClassifier<List<ObservationDiscrete<HmmSequence.Packet>>> classifier
+ /*       ISequenceClassifier<List<ObservationDiscrete<HmmSequence.Packet>>> classifier
                 = new HmmClassifier();
-
+*/
         // WARNING: Remember to change when you have normal data!!!
         // Obfuscation in negative training file?
-        final boolean bObfuscateNeg = true;
-        FASTAObfuscatorReader r = new FASTAObfuscatorReader();
+//       final boolean bObfuscateNeg = true;
+//        FASTAObfuscatorReader r = new FASTAObfuscatorReader();
         //for each file do the same work: train
-        for (int i = 0; i < 3; i++) {
+//        for (int i = 0; i < 3; i++) {
             // Read the sequences
 
             // If obfuscation is on and we are dealing with the negative
             // training file
-            if ((i == 2) && (bObfuscateNeg)) {
+/*            if ((i == 2) && (bObfuscateNeg)) {
                 //FASTAObfuscatorReader r = new FASTAObfuscatorReader();
                 lSeqs = r.getSequencesFromFile(fileNames[i]);
                 fileNames[1] = "Not" + fileNames[1]; // Update to indicate different class
@@ -88,9 +172,9 @@ public class CpGIslandDetection {
                 lSeqs = reader.getSequencesFromFile(fileNames[i]);
 
             System.out.println("lSeqs size="+lSeqs.size());
-
+*/
             // Create HMM sequences
-            ISequenceAnalyst<List<ObservationDiscrete<HmmSequence.Packet>>> analyst
+/*            ISequenceAnalyst<List<ObservationDiscrete<HmmSequence.Packet>>> analyst
                     = new HmmAnalyzer();
             List<List<ObservationDiscrete<HmmSequence.Packet>>> lHmmSeqs = analyst.analyze(lSeqs);
 
@@ -106,7 +190,14 @@ public class CpGIslandDetection {
         ISequenceAnalyst<List<ObservationDiscrete<HmmSequence.Packet>>> analyst
                 = new HmmAnalyzer();
         List<List<ObservationDiscrete<HmmSequence.Packet>>> lHmmSeqs = analyst.analyze(lSeqs);
+*/
+        
+        //-------------------------------HMM CLASSIFIER ENDS HERE-----------------------------------------
 
+        
+        /*
+        
+        //----------------------------HMM EVALUATION STARTS-----------------------------------------------
         //System.out.println("size of lHmmSeqs="+ lHmmSeqs.size());
         String str = null;
         String[] savedResults = new String[lHmmSeqs.size()];
@@ -116,7 +207,7 @@ public class CpGIslandDetection {
         int successForCpG = 0, failForCpG = 0, successForNotCpG = 0, failForNotCpG = 0;
         
         // Init identifier
-//        CpGIslandIdentification identifier = new CpGIslandIdentification();
+        // CpGIslandIdentification identifier = new CpGIslandIdentification();
         CpGIslandIdentification identifier = new CpGIslandIdentificationByList("CpG_hg18.fa");
         
         for (int i = 0; i < lHmmSeqs.size(); i++) {
@@ -177,5 +268,8 @@ public class CpGIslandDetection {
         representation = myAnalyst.analyze(lSeqs);
         for(int i=0; i<representation.size();i++)
         nGramGraphClassifier.classify(representation.get(i));
+        
+        
+        */
     }
 }
